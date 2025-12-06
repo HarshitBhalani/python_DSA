7f011e24df020ca0431749664c89ce1bc85b61c6
Harshit Bhalani <HarshitBhalani@users.noreply.github.com>
2025-12-06 21:56:38 +0530

    practices and loops (for,while) and if-elif

---
+++ P_15.py
@@ -0,0 +1,21 @@
+# i=1
+# while i<6:
+#  print(i)
+#  i+=1
+ 
+ 
+# i=1
+# while i<6:
+#      print(i)
+#      if i==3:
+#          break
+#      i+=1
+     
+# for loops
+fruit = ["apple", "banana", "cherry"]
+for x in fruit:
+    print(x)
+    if x== "grape":
+        break 
+    elif x=="banana":
+        continue
