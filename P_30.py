85b13331b14cd200e123c3616871108eb183b3cd
Harshit Bhalani <HarshitBhalani@users.noreply.github.com>
2025-12-07 23:21:39 +0530

    Bubble sort

---
+++ P_30.py
@@ -0,0 +1,9 @@
+mylist = [12,364, 234, 25, 567, 223,23, 111, 90, 923]
+
+n = len(mylist)
+for i in range(n-1):
+  for j in range(n-i-1):
+    if mylist[j] > mylist[j+1]:
+      mylist[j], mylist[j+1] = mylist[j+1], mylist[j]
+
+print(mylist)
