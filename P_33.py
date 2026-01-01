3953efadf1708cc1941c2c4c15ffbccd2cc2632c
Harshit Bhalani <HarshitBhalani@users.noreply.github.com>
2026-01-01 18:26:00 +0530

    Memory-Efficient LRU Cache

---
+++ P_33.py
@@ -0,0 +1,53 @@
+class Node:
+    
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+
+
+
+class LRUCache:
+    
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.cache = {}
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+
+    def _remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    def _insert(self, node):
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
+
+
+    def get(self, key):
+        if key in self.cache:
+            self._remove(self.cache[key])
+            self._insert(self.cache[key])
+            return self.cache[key].value
+        return -1
+
+    def put(self, key, value):
+        if key in self.cache:
+            self._remove(self.cache[key])
+
+        node = Node(key, value)
+        self._insert(node)
+        self.cache[key] = node
+
+
+        if len(self.cache) > self.capacity:
+            lru = self.tail.prev
+            self._remove(lru)
+            del self.cache[lru.key]
